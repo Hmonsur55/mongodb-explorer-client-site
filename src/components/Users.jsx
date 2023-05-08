50beea7362d89011bd977bdea5ddf05eaedfe2c9
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-08 12:14:06 +0600

    add delete method

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,14 +3,28 @@ import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
-    const users = useLoaderData()
+  const users = useLoaderData()
+  
+  // for delete property
+  const handleDelete = id => {
+    console.log(id)
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+
     return (
       <div>
         <h3>Users length {users.length}</h3>
         {users.map((data) => (
           <p key={data._id}>
-            <h4> {data.name}</h4>
-            <h4>{data.email}</h4>
+            {data.name} : {data.email} : {data._id}{" "}
+            <button onClick={() => handleDelete(data._id)}>X</button>
           </p>
         ))}
       </div>
